26883ac258cbd890815860cd8dfbdba7a1547c71
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-23 16:24:39 +0300

    adds class credentials

--- pslock.py
+++ pslock.py
@@ -24,3 +24,40 @@ class User:
         delete_account(saved) method 
         '''
         User.user_list.remove(self)
+
+class Credentials():
+
+    credentials_list = []
+    @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
+
+    def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """        
+        self.account = account
+        self.userName = userName
+        self.password = password
+    
+    def save_details(self):
+        """
+        method to store a new credential to the credentials list
+        """
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        """
+        delete_credentials method that deletes an account credentials from the credentials_list
+        """
+        Credentials.credentials_list.remove(self)
+
+
+    
