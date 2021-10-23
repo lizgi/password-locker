59007422b2d134eeeb2c7d72032906a05bdb7806
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-23 21:54:36 +0300

    test for initilization and saving

--- pslock-test.py
+++ pslock-test.py
@@ -7,7 +7,15 @@ class TestClass(unittest.TestCase):
     A Test class that defines test cases for the User class.
     """
     def setUp(self):
-        """
-        Method that runs before each individual test methods run.
-        """
+        
         self.new_user = User('ElizabethGikonyo','AbZ3thg3')
+
+    def test_init(self):
+        
+        self.assertEqual(self.new_user.username,'ElizabethGikonyo')
+        self.assertEqual(self.new_user.password,'AbZ3thg3')
+
+    def test_save_user(self):
+    
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
