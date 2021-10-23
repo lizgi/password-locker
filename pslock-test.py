307915fd4808aa0be64d82638a8a6be4761419ab
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-23 21:59:10 +0300

    test for credentials input

--- pslock-test.py
+++ pslock-test.py
@@ -3,9 +3,7 @@ from pslock import User
 from pslock import Credentials
 
 class TestClass(unittest.TestCase):
-    """
-    A Test class that defines test cases for the User class.
-    """
+
     def setUp(self):
         
         self.new_user = User('ElizabethGikonyo','AbZ3thg3')
@@ -19,3 +17,15 @@ class TestClass(unittest.TestCase):
     
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+    
+    def setUp(self):
+        
+        self.new_credential = Credentials('Gmail','gikonyo.elizabeth','op5Gij43')
+    def test_init(self):
+        
+        self.assertEqual(self.new_credential.account,'Gmail')
+        self.assertEqual(self.new_credential.userName,'gikonyo.elizabeth')
+        self.assertEqual(self.new_credential.password,'op5Gij43')
+
