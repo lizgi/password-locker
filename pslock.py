22fb515b2fdf7333fb23c8b20a6f9b5fdf8271fc
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-23 22:45:26 +0300

    identings

--- pslock.py
+++ pslock.py
@@ -7,16 +7,11 @@ class User:
     user_list = []
 
     def __init__(self, username, password):
-        """
-        method for user property
-        """
+       
         self.username = username
         self.password = password
 
     def save_user(self):
-        """
-         method to save user
-        """
         User.user_list.append(self)
     
     @classmethod
@@ -24,77 +19,59 @@ class User:
         return cls.user_list
 
     def delete_user(self):
-        '''
-        delete_account(saved) method 
-        '''
-        User.user_list.remove(self)
+      User.user_list.remove(self)
 
 class Credentials():
-
     credentials_list = []
+
     @classmethod
     def verify_user(cls,username, password):
-        """
-        method for verification
-        """
         a_user = ""
         for user in User.user_list:
             if(user.username == username and user.password == password):
                     a_user == user.username
         return a_user
 
+
     def __init__(self,account,userName, password):
-        """
-        method for credentials to be stored
-        """        
         self.account = account
         self.userName = userName
         self.password = password
+
     
     def save_details(self):
-        """
-        method to store a new credential to the credentials list
-        """
         Credentials.credentials_list.append(self)
 
+
     def delete_credentials(self):
-        """
-        delete_credentials method that deletes an account credentials from the credentials_list
-        """
         Credentials.credentials_list.remove(self)
+
 
     @classmethod
     def find_credential(cls, account):
-        """
-        Method that takes in a account_name and returns a credential that matches that account_name.
-
-        """
         for credential in cls.credentials_list:
             if credential.account == account:
                 return credential
+
+
     @classmethod
     def copy_password(cls,account):
         found_credentials = Credentials.find_credential(account)
         pyperclip.copy(found_credentials.password)
 
+
     @classmethod
-    def if_credential_exist(cls, account):
-        """
-        Method that checks if a credential exists from the credential list and returns true or false.
-        """
-        
-                
+    def if_credential_exist(cls, account):    
         for credential in cls.credentials_list: 
           if credential.account == account:
            return True
         return False
+
+
     @classmethod
     def display_credentials(cls):
-        """
-        Method that returns all items in the credentials list
-
-        """
         return cls.credentials_list
+        
 
     def generatePassword(stringLength=8):
         """Generate a random password string of letters and digits and special characters"""
